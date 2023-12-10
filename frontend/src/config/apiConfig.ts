8b7188309240d58dfc36166917991a61a2324694
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-12-10 14:58:22 +0900

    Fix base url

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,2 +1,6 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000/api";
+
+export const BASE_URL = import.meta.env.GITHUB_ACTIONS
+  ? import.meta.env.BASE_URL
+  : "/";
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { BASE_URL } from "./config/apiConfig";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
