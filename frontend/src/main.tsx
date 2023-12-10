23f1734dde0b59ac2818de8519e7ef09f887aa8f
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-12-10 15:17:41 +0900

    Change deploy flow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { BASE_URL } from "./config/apiConfig";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
